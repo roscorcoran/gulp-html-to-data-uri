2b706c4b7d9c596f1996ae6d3527030997157ded
Ros Corcoran <ros@rocksteady.com>
2018-02-14 14:54:33 +0000

    Fix data URI for Firefox

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var chalk = require('chalk');
 module.exports = function (opts) {
   var count = 0;
   return through.obj(function (file, enc, cb) {
-    gutil.log('The data URI:')
-    var dataUri = 'data: text/html, ' + encodeURIComponent(file.contents.toString());
+    gutil.log('The data URI:');
+    var dataUri = 'data:text/html, ' + encodeURIComponent(file.contents.toString());
     gutil.log(chalk.green(dataUri));
     file.contents = new Buffer(dataUri);
     count++;
